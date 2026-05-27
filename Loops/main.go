1902a194a776639d33dc23a97f97befb8919e1fc
cypherpulse <johnbradill67@gmail.com>
2026-05-27 05:23:23 +0300

    Remove commented-out code for index and name in slice iteration in main.go

--- Loops/main.go
+++ Loops/main.go
@@ -18,7 +18,11 @@ func main() {
 
 	// looping though a slice
 	names := []string{"Alice", "Bob", "Charlie", "David"}
-	for index, name := range names {
-		fmt.Printf("Index: %d, Name: %s\n", index, name) // %d is for integer and %s is for string
+	// for index, name := range names {
+	// 	fmt.Printf("Index: %d, Name: %s\n", index, name) // %d is for integer and %s is for string
+	// }
+
+	for _, name := range names { // if we don't need the index we can use _ to ignore it
+		fmt.Printf("Name: %s\n", name)
 	}
 }
