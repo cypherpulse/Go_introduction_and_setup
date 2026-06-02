6112ec61c9985e052c71b1c9830dedb48dea5120
cypherpulse <johnbradill67@gmail.com>
2026-06-03 00:08:53 +0300

    Fix updateName function to accept a pointer and update name correctly

--- Pointers/main.go
+++ Pointers/main.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 )
 
-func updateName(x string) {
-	x = "wedge"
+func updateName(x *string) {
+	*x = "wedge"
 }
 
 func main() {
 	name := "tifa"
-	updateName(name)
+	// updateName(&name)
+	fmt.Println("Memeory address of name:", &name)
 
-	
-	fmt.Println(name)
+	m := &name
+	// fmt.Println("Memeory address of m:", m)
+	// fmt.Println("Value of m:", *m)
+
+	fmt.Println("Before updateName:", name)
+	updateName(m)
+	fmt.Println("After updateName:", name)
+
+	// fmt.Println(name)
 }
